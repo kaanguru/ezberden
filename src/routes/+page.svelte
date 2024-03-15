e79afac9807e0b1c79e18632d0dc47b45b6cea36
kaanguru <kaanguru@mail.ru>
2024-03-15 12:34:13 +0300

    "Updated +page.svelte to use `on:click` instead of `onclick` for button elements."

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -113,8 +113,8 @@
 		<ShoppingBasket class="end-45 absolute top-1 z-10 text-accent" />
 		<ul>
 			{#each sepettekiParolalar as parola}
-				<li>
-					<button on:click={sepeteTiklama(parola)}>{parola}</button>
+				<li on:click={sepeteTiklama(parola)}>
+					{parola}
 				</li>
 			{/each}
 		</ul>

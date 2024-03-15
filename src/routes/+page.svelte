0dc840b41bca3123798e8d728770003075ae2392
kaanguru <kaanguru@mail.ru>
2024-03-15 11:25:13 +0300

    Update +page.svelte: Replace `hover:text-accent` with `hover:text-accent-foreground` for the `li` element.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -292,7 +292,7 @@
 	}
 	#basket > ul > li {
 		@apply block w-full border-b border-gray-200 px-4 py-2;
-		@apply cursor-pointer hover:bg-gray-100 hover:text-accent;
+		@apply cursor-pointer hover:bg-gray-100 hover:text-accent-foreground;
 		@apply focus:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700;
 	}
 

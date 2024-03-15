0bdccfe5a342f00301ebdb3fe6dcff7dda53165c
kaanguru <kaanguru@mail.ru>
2024-03-15 11:21:20 +0300

    "Update ShoppingBasket position from 'left-2' to 'end-0' and update the class from 'text-secondary-foreground' to 'text-secondary-foreground' for better responsiveness."

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -121,7 +121,7 @@
 </script>
 
 {#if sepettekiParolalar.length > 0}
-	<ShoppingBasket class="absolute left-2 top-0 z-10 text-secondary-foreground" />
+	<ShoppingBasket class="absolute end-0 top-0 z-10 text-secondary-foreground" />
 	<div id="basket">
 		<ul>
 			{#each sepettekiParolalar as parola}

d69287bac181e704093d597dff5e362ce7d15e0d
kaanguru <kaanguru@mail.ru>
2024-04-01 13:40:55 +0300

    feat: create +page.svelte

---
+++ src/routes/gizlilik/+page.svelte
@@ -0,0 +1,47 @@
+<div class="container mx-auto justify-center text-center">
+	<p><strong>Gizlilik Politikası</strong></p>
+	<p>
+		Bu Gizlilik Politikası, Ezberden tarafından toplanan, işlenen ve paylaşılan kişisel verilerin
+		nasıl ele alındığını açıklar. Bu politika, kullanıcıların verilerinin gizliliği ve güvenliği
+		konusunda bilgi sahibi olmalarını sağlamak için hazırlanmıştır.
+	</p>
+	<p><strong>1. Veri Toplama</strong></p>
+	<p>
+		Uygulama, kullanıcıların şifre oluşturma işlemlerini gerçekleştirmek için gerekli minimum
+		verileri toplar. Bu veriler genellikle kullanıcının cihazınızın bilgilerini ve kullanıcının
+		şifre oluşturma işlemlerini içerir.
+	</p>
+	<p><strong>2. Veri Kullanımı</strong></p>
+	<p>
+		Toplanan veriler, şifre oluşturma işlemlerini gerçekleştirmek ve kullanıcı deneyimini
+		iyileştirmek için kullanılır. Veriler, uygulamanın işlevselliğini sağlamak ve kullanıcıların
+		deneyimini iyileştirmek için analiz edilir.
+	</p>
+	<p><strong>3. Veri Paylaşımı</strong></p>
+	<p>
+		Uygulama, toplanan verileri üçüncü taraflarla paylaşmaz. Ancak, hukuka tabi olan durumlar
+		dışında, verilerin güvenliğini sağlamak için gerekli olan teknik ve yönetmelik önlemlerini
+		alırız.
+	</p>
+	<p><strong>4. Veri Güvenliği</strong></p>
+	<p>
+		Uygulama, toplanan verilerin güvenliğini korumak için güçlü şifreleme teknikleri kullanır.
+		Ayrıca, veri güvenliği ile ilgili en iyi uygulamaları takip ederiz.
+	</p>
+	<p><strong>5. Veri Erişimi ve İstek</strong></p>
+	<p>
+		Kullanıcılar, verilerinin erişimini, düzenlenmesini veya silinmesini talep edebilirler. Bu
+		talepler, uygulamanın gizlilik politikasına uygun bir şekilde ele alınır.
+	</p>
+	<p><strong>6. Çocukların Verileri</strong></p>
+	<p>
+		Uygulama, 13 yaşın altındaki çocukların verilerini toplamaz. Eğer çocuk veri toplama durumu
+		ortaya çıkarsa, bu verilerin hemen hemen silinir.
+	</p>
+	<p><strong>7. Gizlilik Politikası Güncellemeleri</strong></p>
+	<p>
+		Bu Gizlilik Politikası, uygulamanın gizlilik uygulamalarını güncellemek için düzenlenebilir.
+		Kullanıcılar, uygulamanın güncel Gizlilik Politikasını ziyaret ederek bu güncellemeleri
+		görebilirler.
+	</p>
+</div>
